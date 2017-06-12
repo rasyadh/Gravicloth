f8dd86c4ab7c99c8545fe037e96a58b19399d629
RASYADH\Rasyadh A Aziz <rasyadhabdulaziz@gmail.com>
2017-06-13 02:27:15 +0700

    fixing dashboard transaction

--- dashboard/transaksi.php
+++ dashboard/transaksi.php
@@ -20,14 +20,13 @@
         <tbody>
           <?php
             $pdo = Database::connect();
-            $sql = 'SELECT t.id_transaction, u.name, t.total_payment, t.transaction_date, t.status_payment FROM transaction t, user u WHERE t.id_user = u.id_user ORDER BY id_transaction ASC';
+            $sql = 'SELECT t.id_transaction, u.name, t.total_payment, t.transaction_date FROM transaction t, user u WHERE t.id_user = u.id_user ORDER BY id_transaction ASC';
             foreach ($pdo->query($sql) as $row){
               echo '<tr>';
               echo '<td>'. $row['id_transaction'] . '</td>';
               echo '<td>'. $row['name'] . '</td>';
               echo '<td>'. $row['total_payment'] . '</td>';
               echo '<td>'. $row['transaction_date'] . '</td>';
-              echo '<td>'. $row['status_payment'] .'</td>';
               echo '<td>'.'<a class="ui green button" onclick="detail()">Detail</a>';
               echo '<a class="ui red button" onclick="del()">Delete</a>';
               echo '</td></tr>';
